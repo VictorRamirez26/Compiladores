586b55b815f01ef5f666767980f63db1349bd178
Victor Ramirez <victorriver6@hotmail.com>
2025-03-24 00:26:55 -0300

    TokenClassifier para digitos,identificadores de variable y keywords

---
+++ src/main/java/compiler/lexer/TokenClassifier.java
@@ -0,0 +1,88 @@
+package compiler.lexer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TokenClassifier {
+
+    private final Map<String, TokenType> keywords = new HashMap<>();
+
+    private void initKeywords() {
+        keywords.put("class", TokenType.KW_CLASS);
+        keywords.put("if", TokenType.KW_IF);
+        keywords.put("impl", TokenType.KW_IMPL);
+        keywords.put("else", TokenType.KW_ELSE);
+        keywords.put("false", TokenType.KW_FALSE);
+        keywords.put("fn", TokenType.KW_FN);
+        keywords.put("ret", TokenType.KW_RET);
+        keywords.put("while", TokenType.KW_WHILE);
+        keywords.put("true", TokenType.KW_TRUE);
+        keywords.put("new", TokenType.KW_NEW);
+        keywords.put("nil", TokenType.KW_NIL);
+        keywords.put("st", TokenType.KW_ST);
+        keywords.put("self", TokenType.KW_SELF);
+        keywords.put("pub", TokenType.KW_PUB);
+        keywords.put("div", TokenType.KW_DIV);
+    }
+
+    public TokenClassifier() {
+        initKeywords();
+    }
+
+    public Token classifyToken(Lexeme lexeme){
+
+        String data = lexeme.getData();
+
+        if (data.isEmpty()){
+            return null;
+        }
+
+        char c = data.charAt(0);
+
+        if (Character.isLowerCase(c)) {
+            return classifyKeywordOrIdentifier(lexeme);
+        } else if (Character.isUpperCase(c)) {
+            return classifyClassIdentifier(lexeme);
+        } else if (Character.isDigit(c)) {
+            return classifyIntOrDouble(lexeme);
+        }
+
+        return null;
+    }
+
+    private Token classifyIntOrDouble(Lexeme lexeme){
+        Token token = setValues(new Token() , lexeme);
+        token.setTokenType(TokenType.INT_CONSTANT); // Por default es INT
+        String data = lexeme.getData();
+        int size = data.length();
+
+        for (int i=0 ; i < size; i++){
+            if (i+1 < size) {
+                if (data.charAt(i) == '.' && Character.isDigit(data.charAt(i+1))) { // Pasa a ser DOUBLE
+                    token.setTokenType(TokenType.DOUBLE_CONSTANT);
+                    break;
+                }
+            }
+        }
+
+        return token;
+    }
+    private Token classifyClassIdentifier(Lexeme lexeme){
+        Token token = setValues(new Token() , lexeme);
+        token.setTokenType(TokenType.IDENTIFIER_CLASS);
+        return token;
+    }
+    private Token classifyKeywordOrIdentifier(Lexeme lexeme){
+        Token token = setValues(new Token() , lexeme);
+        token.setTokenType(keywords.getOrDefault(lexeme.getData(), TokenType.IDENTIFIER_OBJECT));
+        return token;
+    }
+
+    private Token setValues(Token token, Lexeme lexeme){
+        token.setValue(lexeme.getData());
+        token.setLine(lexeme.getLineIndex());
+        token.setColumn(lexeme.getColumnIndex());
+        return token;
+    }
+
+}
--- src/main/java/compiler/lexer/TokenType.java
+++ src/main/java/compiler/lexer/TokenType.java
@@ -1,12 +1,31 @@
 package compiler.lexer;
 
 public enum TokenType {
-    IDENTIFIER,
-    KEYWORD,
-    OPERATOR,
-    SPECIAL_SYMBOL,
+
+    KW_CLASS,
+    KW_IF,
+    KW_IMPL,
+    KW_ELSE,
+    KW_FALSE,
+    KW_FN,
+    KW_RET,
+    KW_WHILE,
+    KW_TRUE,
+    KW_NEW,
+    KW_NIL,
+    KW_ST,
+    KW_SELF,
+    KW_PUB,
+    KW_DIV,
+
+
+    IDENTIFIER_CLASS,
+    IDENTIFIER_OBJECT,
+
     INT_CONSTANT,
     DOUBLE_CONSTANT,
+    OPERATOR,
+    SPECIAL_SYMBOL,
     STRING_CONSTANT,
     BOOLEAN
 }
